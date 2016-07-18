6777ad19a37bb1680656947e2130f66bd1e473e7
stephen.elston <stephen.elston@quantia.com>
2016-07-17 17:25:45 -0700

    Simplified the graphics functions

--- Lecture4/Bootstrap.R
+++ Lecture4/Bootstrap.R
@@ -13,35 +13,34 @@
 ##---------------------------------------------------------------
 library(HistData)
 library(resample)
+library(simpleboot)
 
 male = GaltonFamilies[GaltonFamilies$gender == 'male',]
 female = GaltonFamilies[GaltonFamilies$gender == 'female',]
 
-plot.t <- function(a, b, cols = c('pop_A', 'pop_B'), nbins = 80, p = 0.05){
-  maxs = max(c(max(a), max(b)))
-  mins = min(c(min(a), min(b)))
-  breaks = seq(maxs, mins, length.out = (nbins + 1))
-  par(mfrow = c(2, 1))
-  hist(a, breaks = breaks, main = paste('Histogram of', cols[1]), xlab = cols[1])
-  abline(v = mean(a), lwd = 4, col = 'red')
-  abline(v = quantile(a, probs = p/2), lty = 3, col = 'red', lwd = 3)  
-  abline(v = quantile(a, probs = (1 - p/2)), lty = 3, col = 'red', lwd = 3)
-  hist(b, breaks = breaks, main = paste('Histogram of', cols[2]), xlab = cols[2])
-  abline(v = mean(b), lwd = 4, col = 'red')
-  abline(v = quantile(b, probs = p/2), lty = 3, col = 'red', lwd = 3)  
-  abline(v = quantile(b, probs = (1 - p/2)), lty = 3, col = 'red', lwd = 3)
-  par(mfrow = c(1, 1))
-}
 
-plot.diff <- function(a, cols = 'difference of means', nbins = 80, p = 0.05){
-  maxs = max(a)
-  mins = min(a)
+plot.hist <- function(a, maxs, mins, cols = 'difference of means', nbins = 80, p = 0.05) {
   breaks = seq(maxs, mins, length.out = (nbins + 1))
   hist(a, breaks = breaks, main = paste('Histogram of', cols), xlab = cols)
   abline(v = mean(a), lwd = 4, col = 'red')
   abline(v = 0, lwd = 4, col = 'blue')
   abline(v = quantile(a, probs = p/2), lty = 3, col = 'red', lwd = 3)  
   abline(v = quantile(a, probs = (1 - p/2)), lty = 3, col = 'red', lwd = 3)
+}
+
+plot.t <- function(a, b, cols = c('pop_A', 'pop_B'), nbins = 80, p = 0.05){
+  maxs = max(c(max(a), max(b)))
+  mins = min(c(min(a), min(b)))
+  par(mfrow = c(2, 1))
+  plot.hist(a, maxs, mins, cols = cols[1])
+  plot.hist(b, maxs, mins, cols = cols[2])
+  par(mfrow = c(1, 1))
+}
+
+plot.diff <- function(a, cols = 'difference of means', nbins = 80, p = 0.05){
+  maxs = max(a)
+  mins = min(a)
+  plot.hist(a, maxs, mins, cols = cols[1])
 }
 
 ## Bootstrap the mean of the sons and of daughters
