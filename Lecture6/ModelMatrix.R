52446ee7020303829ac36749461b010b38774d86
stephen.elston <stephen.elston@quantia.com>
2016-08-01 16:32:13 -0700

    Initial commit

---
+++ Lecture6/ModelMatrix.R
@@ -0,0 +1,21 @@
+##--------------------------------------------
+##
+## Class: PCE 350 Data Science Methods Class
+##
+##---- Regression with R Part 2 ----
+##
+##----- Model matrix with categorical variables ---
+##
+
+unique(auto.price$drive.wheels) # How many values?
+
+## First attempt to build a simple model matrix
+mod.mat = model.matrix(lnprice ~ drive.wheels, data = auto.price)
+head(mod.mat)  # Note the use of contrast to intercept
+
+## Or, code without the intercept
+mod.mat = model.matrix(lnprice ~ 0 + drive.wheels, data = auto.price)
+head(mod.mat)
+
+
+
